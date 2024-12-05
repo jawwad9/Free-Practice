e127112b690614a8ebdd60d01d6896cbd65c18bf
jawwad9 <jawwadmuhammad514@gmail.com>
2024-12-05 23:49:23 +0500

    complete

---
+++ 02/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv"
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+
+app.get("/home", (req, res) => {
+    res.send("Hello jawwad!");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
