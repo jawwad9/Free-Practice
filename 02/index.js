7cff52ec428feaf203c442b2cc392bd3b90306a2
Muhammad Jawwad <154079885+jawwad9@users.noreply.github.com>
2025-01-08 21:00:26 +0500

    first commit

--- 02/index.js
+++ 02/index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello jawwad!");
 })
 
 app.get("/profile", (req, res) => {
