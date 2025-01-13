09ba3ac99a1ea10a177089ffaad2180699567c77
Muhammad Jawwad <154079885+jawwad9@users.noreply.github.com>
2025-01-13 21:50:43 +0500

    post page model complete

--- 02/index.js
+++ 02/index.js
@@ -34,12 +34,6 @@ app.get("/about", (req, res) => {
     res.send("Hello about!");
 })
 
-app.get("/about", (req, res) => {
-    res.send("Hello about!");
-})
-app.get("/about", (req, res) => {
-    res.send("Hello about!");
-})
 
 
 app.listen(port, () => {
