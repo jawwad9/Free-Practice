dff6daafae9b97918687af9ea7624b76b56d9fba
Muhammad Jawwad <154079885+jawwad9@users.noreply.github.com>
2025-01-20 21:04:17 +0500

    complete

--- 02/index.js
+++ 02/index.js
@@ -35,14 +35,6 @@ app.get("/about", (req, res) => {
 })
 
 
-app.get("/about", (req, res) => {
-    res.send("Hello about!");
-})
-
-app.get("/about", (req, res) => {
-    res.send("Hello about!");
-})
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
