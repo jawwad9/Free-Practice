715226c46fad1745dd81cea59052bd998b5904e0
Muhammad Jawwad <154079885+jawwad9@users.noreply.github.com>
2025-01-27 22:52:17 +0500

    create models

--- 02/index.js
+++ 02/index.js
@@ -34,6 +34,13 @@ app.get("/about", (req, res) => {
     res.send("Hello about!");
 })
 
+app.get("/about", (req, res) => {
+    res.send("Hello about!");
+})
+app.get("/about", (req, res) => {
+    res.send("Hello about!");
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
