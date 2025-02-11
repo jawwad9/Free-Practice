6fe250f5f429a8c1a36087420e9ccfde5267566b
Muhammad Jawwad <154079885+jawwad9@users.noreply.github.com>
2025-02-11 19:36:54 +0500

    complete

--- 01 ruf/index.js
+++ 01 ruf/index.js
@@ -9,18 +9,6 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 })
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
-
 
 
 app.listen(port, () => {
